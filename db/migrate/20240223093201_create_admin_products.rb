5d065061d334cc9f3391970e7cba4f3dcd65c49f
mkawsar <kawsar.diu888@gmail.com>
2024-02-23 15:44:21 +0600

    product table migration

---
+++ db/migrate/20240223093201_create_admin_products.rb
@@ -0,0 +1,14 @@
+class CreateAdminProducts < ActiveRecord::Migration[7.1]
+    def change
+        create_table :products do |t|
+            t.string :name
+            t.text :description
+            t.integer :price
+            t.references :category, null: false, foreign_key: true
+            t.integer :stock
+            t.boolean :active
+
+            t.timestamps
+        end
+    end
+end
