22c9877d3876735a0aaf858a31fb5e79b22e4290
mkawsar <kawsar.diu888@gmail.com>
2024-02-24 14:07:11 +0600

    product stock added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :products
-  end
+    namespace :admin do
+        resources :products do
+            resources :stocks
+        end
+    end
     namespace :admin do
         resources :categories
     end
