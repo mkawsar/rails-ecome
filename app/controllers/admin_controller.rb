3f03ba56423ce8c8012647fcb5544164ed004743
mkawsar <kawsar.diu888@gmail.com>
2024-03-19 11:54:27 +0600

    order dashboard statistic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,13 @@
 class AdminController < ApplicationController
     before_action :authenticate_admin!
+
     def index
+        @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+        @quick_stats = {
+            sales: Order.where(created: Time.now.midnight..Time.now).count,
+            revenue: Order.where(created: Time.now.midnight..Time.now).sum(:total).round,
+            avg_sale: Order.where(created: Time.now.midnight..Time.now).average(:total),
+            per_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight..Time.now }).average(:quantity)
+        }
     end
 end
